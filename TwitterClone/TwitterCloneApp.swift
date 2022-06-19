b8ff0f5e332dd6f0b71090f01f52ff71f3169bdb
Leonardo Albergaria <leonardo.albergaria@inpeaceapp.com>
2022-06-19 12:08:17 -0300

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Leonardo Albergaria on 19/06/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
