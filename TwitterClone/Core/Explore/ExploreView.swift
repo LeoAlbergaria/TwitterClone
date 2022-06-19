1798892d1c3a7ec7b22ebab48fe0a12c268bb445
Leonardo Albergaria <leonardo.albergaria@inpeaceapp.com>
2022-06-19 13:13:00 -0300

    Main Tab added

--- TwitterClone/Core/Explore/ExploreView.swift
+++ TwitterClone/Core/Explore/ExploreView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  ExploreView.swift
 //  TwitterClone
 //
 //  Created by Leonardo Albergaria on 19/06/22.
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ExploreView: View {
     var body: some View {
-        MainTabView()
+        Text("Explore View")
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ExploreView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ExploreView()
     }
 }
---
+++ TwitterClone/Core/Notifications/NotificationsView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationsView.swift
+//  TwitterClone
+//
+//  Created by Leonardo Albergaria on 19/06/22.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        Text("Notifications View")
+    }
+}
+
+struct NotificationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationsView()
+    }
+}
